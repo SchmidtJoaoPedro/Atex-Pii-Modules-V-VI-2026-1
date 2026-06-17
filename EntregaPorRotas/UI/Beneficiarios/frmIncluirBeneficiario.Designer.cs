55742d60d34c4c71ff0c1358ac67eb1842936652
SchmidtJoaoPedro <joaoschmidtmagalhaes@gmail.com>
2026-06-17 16:37:22 -0300

    fix: frm add beneficiarios

---
+++ EntregaPorRotas/UI/Beneficiarios/frmIncluirBeneficiario.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace EntregaPorRotas.UI
+{
+    partial class frmIncluirBeneficiario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmIncluirBeneficiario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1540, 769);
+            this.Name = "frmIncluirBeneficiario";
+            this.Text = "frmIncluirBeneficiario";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
