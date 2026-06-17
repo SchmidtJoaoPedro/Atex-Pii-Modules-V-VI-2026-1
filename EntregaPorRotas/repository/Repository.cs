356f9003768d218afb1d93e1560c23b3bc040773
Rafael Engel <rafaelengelserafim09@gmail.com>
2026-06-17 16:39:39 -0300

    repositórios e objetos criados

---
+++ EntregaPorRotas/repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntregaPorRotas.repository
+{
+    internal class Repository
+    {
+        // lista todos de uma tabela referenciada
+        public DataTable ListarTodos(string tabela)
+        {
+            if (tabela == null || tabela = string.Empty) 
+            {
+                return null;
+            }
+
+            SqlConnection conn = Conexao.Conectar();
+            string sql = @"SELECT * FROM @Tabela";
+            SqlCommand cmd = new SqlCommand(conn, sql);
+            cmd.Parameters.AddWithValue("@Tabela", tabela);
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+    }
+}
